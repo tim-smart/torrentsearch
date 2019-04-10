8f98fbf01a315414ced86f45fbfee9b104bb3a8e
Tim Smart <tim.smart@arisechurch.com>
2019-04-10 13:24:59 +1200

    Move to prompt based input

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,17 @@
 ///<reference path="torrent-search-api.d.ts"/>
 import { Command, flags } from "@oclif/command";
 import chalk from "chalk";
-import cli from "cli-ux";
 import * as fs from "fs-extra";
+import * as inquirer from "inquirer";
+import {} from "open";
+import open = require("open");
 import { join } from "path";
 import * as path from "path";
 import {
   disableProvider,
   enableProvider,
   enablePublicProviders,
+  getActiveProviders,
   getMagnet,
   loadProviders,
   search,
@@ -59,36 +62,75 @@ class Torrentsearch extends Command {
       );
     });
 
+    await this.selectProviders();
+    const query = await this.getQuery(args);
+
     const torrents = await search(
-      args.query,
+      query,
       parsedFlags.category,
       parsedFlags.limit!,
     );
-    for (const torrent of torrents) {
-      await this.logTorrent(torrent);
-    }
+
+    await this.selectTorrent(torrents);
   }
 
-  private async logTorrent(torrent: Torrent) {
-    this.log(chalk.bold(torrent.title));
-    this.log(
-      `${chalk.bold.blue(torrent.size)} - ` +
-        `${chalk.bold.green("S:")} ${torrent.seeds} - ` +
-        `${chalk.bold.green("P:")} ${torrent.peers} - ` +
-        `${torrent.time}`,
-    );
-    if (torrent.magnet) {
-      cli.url("magnet", torrent.magnet);
-    } else if (torrent.link) {
-      cli.url("torrent dl", torrent.link);
-    } else {
-      const magnet = await getMagnet(torrent);
-      if (magnet) {
-        cli.url("magnet", magnet);
-      } else {
-        cli.url("details", torrent.desc);
-      }
+  private async selectProviders() {
+    const providers = getActiveProviders();
+    const results = await inquirer.prompt<{ providers: string[] }>([
+      {
+        message: "Select providers",
+        name: "providers",
+        type: "checkbox",
+
+        choices: providers.map(p => ({
+          checked: true,
+          value: p.name,
+        })),
+      },
+    ]);
+    providers
+      .filter(p => !results.providers.includes(p.name))
+      .forEach(p => disableProvider(p.name));
+  }
+
+  private async getQuery(args: any): Promise<string> {
+    if (args.query) {
+      return args.query;
     }
+
+    const result = await inquirer.prompt<{ query: string }>({
+      message: "Search query",
+      name: "query",
+      type: "input",
+    });
+    return result.query;
+  }
+
+  private torrentRow(torrent: Torrent) {
+    return (
+      `${chalk.yellow(torrent.title)} ` +
+      `${chalk.blue(torrent.size)} ` +
+      `${chalk.green((torrent.seeds || 0).toString())} ` +
+      `${chalk.red((torrent.peers || 0).toString())} ` +
+      `${torrent.time}`
+    );
+  }
+
+  private async selectTorrent(torrents: Torrent[]) {
+    const result = await inquirer.prompt<{ torrent: Torrent }>({
+      message: "Select a result",
+      name: "torrent",
+      pageSize: 15,
+      type: "rawlist",
+
+      choices: torrents.map(t => ({
+        name: this.torrentRow(t),
+        value: t,
+      })),
+    });
+
+    const magnet = await getMagnet(result.torrent);
+    await open(magnet || result.torrent.link || result.torrent.desc);
   }
 }
 
